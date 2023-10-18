15840dad94dd402dd42a1e17b6bf9554d99a9ec4
lawalgodwin <godwinl200@gmail.com>
2023-10-18 15:25:38 +0100

    List all documents in Python

--- 0x01-NoSQL/8-all.py
+++ 0x01-NoSQL/8-all.py
@@ -1,11 +1,9 @@
 #!/usr/bin/env python3
 """A modile containing a function that lists all documents in a collection"""
-from typing import Mapping, Any, List, TypeVar
+from typing import Mapping, Any, List
 from pymongo.collection import Collection
-
-T = TypeVar("T")
 
 
 def list_all(mongo_collection: Collection) -> List[Mapping[str, Any]]:
     """ Retue a list of all documents in the given collection"""
-    return list(mongo_collection.find({}))
+    return [ doc for doc in mongo_collection.find({})]
