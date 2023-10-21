f7c097b78c2c7604d6e7836c060733514d575f57
lawalgodwin <godwinl200@gmail.com>
2023-10-21 18:40:53 +0100

    Read from Redis and recover original type

---
+++ 0x02-redis_basic/1-main.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python3
+"""
+Main file
+"""
+import redis
+
+Cache = __import__('exercise').Cache
+
+cache = Cache()
+
+# test for task 1
+
+data = b"hello"
+key = cache.store(data)
+print(key)
+
+local_redis = redis.Redis()
+print(local_redis.get(key))
+
+
+# test for task 1
+# The following code should not raise:
+
+TEST_CASES = {
+    b"foo": None,
+    123: int,
+    "bar": lambda d: d.decode("utf-8")
+}
+
+for value, fn in TEST_CASES.items():
+    key = cache.store(value)
+    print(cache.get(key, fn=fn) == value)
