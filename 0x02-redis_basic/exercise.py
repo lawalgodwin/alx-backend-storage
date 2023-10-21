42703a15da1990604300d4d69ca499dc289ddaee
lawalgodwin <godwinl200@gmail.com>
2023-10-21 17:26:20 +0100

    Implement a Cache class with a store method for basic types

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -7,13 +7,13 @@ from typing import Union
 
 
 class Cache:
-    def __init__(self):
+    def __init__(self) -> Redis:
         """Constructor for the Cache class"""
         self.__redis = Redis()
         self.__redis.flushdb()
 
     def store(self, data: Union[str, bytes, int, float]) -> str:
         """Store the given data in redis and returns the key"""
-        key = str(uuid4())
-        self.__redis.set(key, data)
-        return key
+        self.key = str(uuid4())
+        self.__redis.set(self.key, data)
+        return self.key
