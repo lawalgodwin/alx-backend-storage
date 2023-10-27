935774c4bb6d7d2385cbdbf8f027aac97fa8e402
lawalgodwin <godwinl200@gmail.com>
2023-10-27 20:11:50 +0100

    Use view to create a coreport on data

--- 0x00-MySQL_Advanced/11-need_meeting.sql
+++ 0x00-MySQL_Advanced/11-need_meeting.sql
@@ -9,6 +9,6 @@
 --   AND no last_meeting date OR more than a month
 
 CREATE VIEW need_meeting AS
-SELECT * FROM students
+SELECT name FROM students
 WHERE score < 80 
 AND ((last_meeting IS NULL) OR last_meeting < (DATE_SUB(NOW(), INTERVAL 1 MONTH)));
